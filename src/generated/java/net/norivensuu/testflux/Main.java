b5e4ac06ce8c304fbe2164fb47f6269ee2dd6928
AlexNompe <Alex.Nompe@yandex.ru>
2026-01-29 09:44:26 +0300

    update readme and code

--- src/generated/java/net/norivensuu/testflux/Main.java
+++ src/generated/java/net/norivensuu/testflux/Main.java
@@ -3,5 +3,5 @@ float j = 1.0f;
 double k = 1.0;
 double m = 1.0d;
 private float Calculate(float a, float b) {	float c = a + b;	float d = 10;	return c + d;}
-void main() {	Calculate(i, j);	class _Class_TestString$1537358694 {		static String TestString(String name) {			return "Hello, " + name + "!";		}
-    }	String wow = _Class_TestString$1537358694.TestString("random user");	System.out.println(wow);}
+void main() {	Calculate(i, j);	class _Class_TestString$457233904 {		static String TestString(String name) {			return "Hello, " + name + "!";		}
+    }	String wow = _Class_TestString$457233904.TestString("random user");	System.out.println(wow);}
