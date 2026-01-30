563bb8a729d1bb0af70e324332df7722bf740eb6
arukusu iinpu <Alex.Nompe@yandex.ru>
2026-01-30 16:40:57 +0300

    implemented the for loop, slightly better string handling, program declared statements

---
+++ src/generated/java/net/norivensuu/testflux/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+float i = 1.0F;
+float j = 1.0f;
+double k = 1.0;
+double m = 1.0d;
+boolean test;
+private boolean SampleFunction(float a, float b) {	float c = a + b;	float d = 3.14f;	float m = (float) Math.pow(c, d);	boolean k = c < d && d < m;	return k;}
+void main() {	test = SampleFunction(i, j);	class _Class_SampleString$884452399 {		String SampleString(String name) {			return "Hello, " + name + "!";		}
+    }
+    var _class_SampleString$884452399 = new _Class_SampleString$884452399();	String wow = _class_SampleString$884452399.SampleString("random user") + test;	System.out.println(wow);}
